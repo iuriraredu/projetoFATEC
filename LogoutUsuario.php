713b10cd8218be1fbfc801e9adf76fa37e46a524
Iuri Ramos Reducino <64060630+iuriraredu@users.noreply.github.com>
2020-06-18 17:39:10 -0300

    Update LogoutUsuario.php

--- LogoutUsuario.php
+++ LogoutUsuario.php
@@ -1,11 +1,11 @@
 <?php
 	session_start(); // inicia php
-
-  unset( // destroi as variaveis
-    $_SESSION['idUsuario'],
-    $_SESSION['nomeUsuario'],
-    $_SESSION['eMail'],
-  );
-
-  header("Location: index.php"); // retorna pagina de login
+	unset( // destroi as variaveis
+		$_SESSION['idUsuario'],
+		$_SESSION['nomeUsuario'],
+		$_SESSION['eMail'],
+		$_SESSION['caronaAmiga'],
+		$_SESSION['Usuario_TIPO'] 
+  	);
+	header("Location: index.php"); // retorna pagina de login
 ?>
