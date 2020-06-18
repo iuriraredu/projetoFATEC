82ce64700193fc9bf7d6bcc19e7b3fb0792faf26
Iuri Ramos Reducino <64060630+iuriraredu@users.noreply.github.com>
2020-06-18 17:41:47 -0300

    Update AtualizarCadastro.php

--- AtualizarCadastro.php
+++ AtualizarCadastro.php
@@ -3,16 +3,17 @@
 
   include_once("Conexao.php");
 
-  $inputNomeCompleto = filter_input(INPUT_POST, 'inputNomeCompleto', FILTER_SANITIZE_STRING);
-  $inputEmail = filter_input(INPUT_POST, 'inputEmail', FILTER_SANITIZE_EMAIL);
-  $inputSenha = filter_input(INPUT_POST, 'inputSenha', FILTER_SANITIZE_STRING);
-  $inputCaronaAmiga = (filter_input(INPUT_POST, 'inputCaronaAmiga', FILTER_SANITIZE_STRING)) == 'on' ? 1 : 0 ;
-  $inputTipoUsuario = filter_input(INPUT_POST, 'inputTipoUsuario', FILTER_SANITIZE_NUMBER_INT);
-  $codMatricula = 0;
-  $t = intval($_SESSION['idUsuario']);
+  /* --------------------------------------------
+    $inputNomeCompleto = filter_input(INPUT_POST, 'inputNomeCompleto', FILTER_SANITIZE_STRING);
+    $inputEmail = filter_input(INPUT_POST, 'inputEmail', FILTER_SANITIZE_EMAIL);
+    $inputSenha = filter_input(INPUT_POST, 'inputSenha', FILTER_SANITIZE_STRING);
+    $inputCaronaAmiga = (filter_input(INPUT_POST, 'inputCaronaAmiga', FILTER_SANITIZE_STRING)) == 'on' ? 1 : 0 ;
+    $inputTipoUsuario = filter_input(INPUT_POST, 'inputTipoUsuario', FILTER_SANITIZE_NUMBER_INT);
+    $codMatricula = 0;
+    $t = intval($_SESSION['idUsuario']);
 
 
-    /* --------------------------------------------
+    
     echo "Nome Completo: $inputNomeCompleto <br/>";
     echo "E-mail: $inputEmail <br/>";
     echo "Senha: $inputSenha <br/>";
@@ -20,7 +21,6 @@
     echo "Tipo de Usuário: $inputTipoUsuario <br/>";
     echo "Código de Matricula: $codMatricula <br/>";
     echo "Código de Usuário: $t <br/>";
-    -------------------------------------------- */
 
     if ('$inputSenha' == ""){
       $sqlquery = "UPDATE usuario
@@ -39,6 +39,42 @@
                        Usuario_TIPO = $inputTipoUsuario
                    WHERE idUsuario = $t";
     }
+    -------------------------------------------- */
+
+  if($_POST) {
+		$auxiliar = "";
+        $inputSenha          = $_POST['inputSenha'];
+        $inputConfirmaSenha  = $_POST['inputConfirmaSenha'];
+		$inputNomeCompleto = $_POST['inputNomeCompleto'];
+		$inputEmail = filter_input(INPUT_POST, 'inputEmail', FILTER_SANITIZE_EMAIL);
+		$inputCaronaAmiga = (filter_input(INPUT_POST, 'inputCaronaAmiga', FILTER_SANITIZE_STRING)) == 'on' ? 1 : 0 ;
+		$inputTipoUsuario = filter_input(INPUT_POST, 'inputTipoUsuario', FILTER_SANITIZE_NUMBER_INT);
+		$codMatricula = 0;
+		$t = intval($_SESSION['idUsuario']);
+        if ($inputSenha == $auxiliar) {
+			$sqlquery = "UPDATE usuario
+						SET nomeUsuario = '$inputNomeCompleto',
+							eMail = '$inputEmail',
+							caronaAmiga = $inputCaronaAmiga,
+							Usuario_TIPO = $inputTipoUsuario
+						WHERE idUsuario = $t";
+
+            // $_SESSION['msgAtlCad'] = "<b>| AVISO:</b> Senha não foi alterada!</span>";
+        } else if ($inputSenha == $inputConfirmaSenha) {
+			$sqlquery = "UPDATE usuario
+						SET nomeUsuario = '$inputNomeCompleto',
+							eMail = '$inputEmail',
+							senha = '$inputSenha',
+							caronaAmiga = $inputCaronaAmiga,
+							Usuario_TIPO = $inputTipoUsuario
+						WHERE idUsuario = $t";
+			
+            //$_SESSION['msgAtlCad'] = "<span class='sucesso'><b>Sucesso</b>: As senhas são iguais: ".$senha."</span>";
+        } else {
+            $_SESSION['msgAtlCad'] = "<b>|</b> Não foi possivel salvar alteração, verifique a senha :(";
+        }
+    }
+
     $resultado = mysqli_query($conn, $sqlquery);
 
     if (mysqli_affected_rows($conn) > 0){
@@ -51,7 +87,7 @@
     	// echo "Cadastrado com Sucesso!";
     }
     else{
-      $_SESSION['msgAtlCad'] = "<b>|</b> Não Foi Salvo Com Sucesso :(";
+	    $_SESSION['msgAtlCad'] = ($_SESSION['msgAtlCad'] == "") ? "<b>|</b> Não foi possivel salvar alteração :(" : $_SESSION['msgAtlCad'];
       header("Location: Cadastro.php");
       // echo "Não cadastro!";
     }
